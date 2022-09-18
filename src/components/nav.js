994c2a999295abff1197df0f4eb24f190c323ab1
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-09-18 16:34:44 +0530

    Final Trial Commit

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCheckSquare, faCoffee } from "@fortawesome/fontawesome-free-solid";
+import { TypeAnimation } from "react-type-animation";
 
 class Nav extends Component {
   state = {};
@@ -9,6 +10,22 @@ class Nav extends Component {
       <div>
         <FontAwesomeIcon icon={faCoffee}> </FontAwesomeIcon>
         <h1 className="text-green-500">Bobbie</h1>
+        <TypeAnimation
+          sequence={[
+            "One", // Types 'One'
+            1000, // Waits 1s
+            "Two", // Deletes 'One' and types 'Two'
+            2000, // Waits 2s
+            "Two Three", // Types 'Three' without deleting 'Two'
+            () => {
+              console.log("Done typing!"); // Place optional callbacks anywhere in the array
+            },
+          ]}
+          wrapper="div"
+          cursor={true}
+          repeat={Infinity}
+          style={{ fontSize: "2em" }}
+        />
       </div>
     );
   }
