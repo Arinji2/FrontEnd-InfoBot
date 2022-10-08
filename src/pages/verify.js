0f42320a67d042bfc7d7c6281952b40d6da793bb
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-10-08 21:32:21 +0530

    Fix Verification Error

--- src/pages/verify.js
+++ src/pages/verify.js
@@ -11,6 +11,7 @@ function Verify() {
 
   useEffect(() => {
     onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser === null) window.location.assign("/login");
       setUserEmail(currentUser.email);
     });
     if (document.readyState === "complete") {
@@ -61,14 +62,16 @@ function Verify() {
   };
   return (
     <div>
-      <div className="flex flex-col items-center pt-20 m-4">
-        <div className=" flex flex-col items-center">
+      <div className="flex flex-col items-center pt-20 m-4 justify-center">
+        <div className=" flex flex-col items-center justify-center">
           <h1 className="text-4xl text-yellow-400">Verify User</h1>
-          <h1 className="pt-10 text-2xl">
-            Current User: <span className="text-yellow-400">{userEmail}</span>
+          <h1 className="pt-10 text-2xl text-center">
+            Current User: <br></br>
+            <span className="text-yellow-400">{userEmail}</span>
           </h1>
-          <h1 className="pt-10 text-2xl">
-            Verified: <span className="text-yellow-400">{isVerified}</span>
+          <h1 className="pt-10 text-2xl text-center">
+            Verified: <br></br>{" "}
+            <span className="text-yellow-400">{isVerified}</span>
           </h1>
           <button
             className="mt-10 bg-white hover:bg-green-500  text-black-950 rounded-lg p-3 transition-all ease-in-out duration-300"
