ccf5df3040a1998a8287daa378f44e755f735cd6
Arinji2 <87296817+Arinji2@users.noreply.github.com>
2022-10-14 22:53:37 +0530

    New Login

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyBidIYxmER28Jqw4ztc_OEydKbpFN88oBk",
   authDomain: "auth.infobot.uk",
@@ -9,8 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "83891577277",
   appId: "1:83891577277:web:138678e9aeef9371aa72d4",
   measurementId: "G-RSS316Y5YB",
+  storageBucket: "gs://auth-2216a.appspot.com",
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
